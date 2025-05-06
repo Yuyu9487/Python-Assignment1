154982c9c3e2b85d1bc4da45c134f0717b36a7f4
Yuyu9487 <164728799+Yuyu9487@users.noreply.github.com>
2025-05-06 16:10:17 +0800

    Update nurse.py

--- nurse.py
+++ nurse.py
@@ -26,6 +26,7 @@ def Nurse():
                     print("Error. Please Enter A Valid Input.")
 
 def view_doc_appointment():
+    pass
 def view_appointment(): #viwe doctor's 
     nurseinfo = fileManager.readFile("nurseinfo.txt")
     #data = [(id, name, contact, age), (id, name, contact, age), (id, name, contact, age), ..
