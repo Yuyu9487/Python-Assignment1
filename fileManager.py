1f1ebc6e0f807eeae7026d19dcb5b5c12d35e39c
Yuyu9487 <164728799+Yuyu9487@users.noreply.github.com>
2025-05-06 14:47:00 +0800

    FileManger
    完善了fileManeger

--- fileManager.py
+++ fileManager.py
@@ -2,26 +2,33 @@
 # read = 3/#id/#name/#age/#id/#name/#age
 
 def readFile(path : str):
-    with open(path, "r") as file:
-        content = file.read()
-    data = content.split("/#")
-    number = int(data[0])
-    datalist = []
-    group = []
-    groupNumber = 0
-    while groupNumber < len(data) - 1:
-        for i in range(0, number):
-            group.append(data[i + groupNumber + 1])
-        datalist.append(group)
+    try:
+        with open(path, "r") as file:
+            content = file.read()
+        data = content.split("/#")
+        number = int(data[0])
+        datalist = []
         group = []
-        groupNumber += number
-    return datalist
+        groupNumber = 0
+        while groupNumber < len(data) - 1:
+            for i in range(0, number):
+                group.append(data[i + groupNumber + 1])
+            datalist.append(group)
+            group = []
+            groupNumber += number
+        return datalist
+    except:
+        print("\n", path, "can not found!\n")
+        return []
 
-def writeFile(path : str, number : int, data : str):
+def writeFile(path : str, number : int, data):
     write = ""
     write += str(number)
     for group in data:
         for var in group:
             write += "/#" + var
-    with open(path, "w") as file:
-        file.write(write)
+    try:
+        with open(path, "w") as file:
+            file.write(write)
+    except:
+        print("\n", path, "can not found!\n")
--- patient.py
+++ patient.py
@@ -2,41 +2,54 @@ import main
 import fileManager
 #Patient function
 def Patient():
+    print("1.Sign In")
+    print("2.Login")
     print("\n============================\nPatient Menu\n============================")
-    print("1.View Personal Medical Records\n2.View Appointment\n3.Update Infomation\n4.Access Billing Details And Payment History\n5.Back To Menu")
+    print("1.View Personal Medical Records")
+    print("2.View Appointment")
+    print("3.Update Infomation")
+    print("4.Access Billing Details And Payment History")
+    print("5.Back To Menu")
     while True:
         user = input("Your Choice:")
         match user:
-                case "1":
-                    MedicalRec()
-                    break
-                case "2":
-                    Pat_viewappoint()
-                    break
-                case "3":
-                    updateinfo()
-                    break
-                case "4":
-                    Paymenthistory()
-                    break
-                case "5":
-                    main.main()
-                    break
-                case _:
-                    print("Error. Please Enter A Valid Input.")
+            case "1":
+                SignIn()
+                break
+            case "2":
+                Login()
+                break
+            case _:
+                print("Error. Please Enter A Valid Input.")
 
+def SignIn():
+    patientInfo = fileManager.readFile("patient.txt")
+    user = [input("Enter your Name: "), input("Enter your Password: "), input("Enter your Contact Number: ")]
+    i = 0
+    allID = []
+    for info in patientInfo:
+        allID.append(info[0])
+    while i < 10000:
+        if i in allID:
+            i += 1
+        else:
+            patientInfo.append([str(i), user[0], user[1], user[2]])
+            fileManager.writeFile("patient.txt", 4, patientInfo)
+            break
+
+def Login():
+    patientInfo = fileManager.readFile("patient.txt")
+    user = [input("Enter your Name: "), input("Enter your Password: ")]
 def MedicalRec():
     return
 def Pat_viewappoint():
     return
 def updateinfo():
     patientInfo = fileManager.readFile("patient.txt")
-    # data = [(id, name, contact)]
     user = input("Enter your ID: ")
     for info in patientInfo:
         if info[0] == user:
             user = input("Enter your Name: ")
-            info[]
     return
 def Paymenthistory():
     return
