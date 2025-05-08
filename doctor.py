5a67d4d710c1f02f1341688c7c03190326faf830
Ayatsuki <kawwenxuan@gmail.com>
2025-05-08 10:41:44 +0800

    3

--- doctor.py
+++ doctor.py
@@ -1,5 +1,6 @@
 import main
 import fileManager
+import re
 #Doctor Funtion
 MEDICAL_HISTORY_FILE="patient_observation.txt"
 APPOINTMENT_FILE="patient.txt"
@@ -20,7 +21,7 @@ def Doctor():
                     viewappoint()
                     break
                 case "4":
-                    Timetable()
+                    Blocklist()
                     break
                 case "5":
                     main.main()
@@ -53,5 +54,37 @@ def UpdatePatRec():
     
 def viewappoint():
     return
-def Timetable():
-    return
+def Blocklist():
+    user = input("============================\n1.Block Schedule\n2.Unblock Schedule")
+    if user == "" or user.isdigit() == False:
+        print("Invalid input, ID must be digits and cannot be null.")
+    else:
+        user = int(user)
+        match user:
+            case 1:
+                date = input("Enter the date in DD/MM/YY,seperated by '/'")
+                pattern = ("^([1-9]|0[1-9]|[12][0-9]|3[01])/([1-9]|0[1-9]|1[0-2])/(\d{2})$")
+                try: 
+                    if re.match(pattern,date):
+                        Appointment = fileManager.readFile("AppointmentBlockList.txt")
+                        for info in Appointment:
+                            if info == date:
+                                print(f"Date already exist.")
+                                break
+                        if info != date: #already check all, and no repeating date are found
+                            print(f"{date} blocked successfully!")
+                            Appointment.append([info])
+                            fileManager.writeFile("patient.txt", 1, Appointment)
+
+                    else:
+                        raise ValueError("Date does not match DD/MM/YY format or input is invalid")
+                except ValueError as Error:
+                    print(Error)
+
+
+                    
+
+
+
+                
+
--- receptionist.py
+++ receptionist.py
@@ -29,6 +29,7 @@ def Register():
     Entered = False
     while Entered == False:
         patfile = fileManager.readFile("patient.txt")
+        appointment = fileManager.readFile("Appointment.txt")
         regname = input("Enter Patient Name:")
         if regname == "":
             print("Error:Name cannot be blank.")
@@ -58,11 +59,14 @@ def Register():
                         else:
                             Entered = True
                             patfile.append([str(i), regname, regpass, regcontact])
+                            appointment.append([str(i),"Null"])
                             fileManager.writeFile("patient.txt", 4, patfile)
-                            print("Patient added successfully!")
-                            Receptionist()
+                            fileManager.writeFile("Appointment.txt",2,appointment)
+                            print(f"Patient ID: {i} added successfully!")
+                            
                             break
                     break
+    Receptionist()
 
 def UpdatePatDes():
     patientInfo = fileManager.readFile("patient.txt")
@@ -73,7 +77,12 @@ def UpdatePatDes():
         for info in patientInfo:
             if info[0] == userid:
                 print(f"User {info[1]} found.")
-                user = int(input("What would you like to Update?\n1.Name\n2.Password\n3.Contact Number\n4.Abort\nYour Choice:"))
+                user = input("============================\nWhat would you like to Update?\n1.Name\n2.Password\n3.Contact Number\n4.Abort\nYour Choice:")
+                if user == "" or user.isdigit() == False:
+                    print("Invalid input, ID must be digits and cannot be null.")
+                    Receptionist()
+                else:
+                    user = int(user)
                 if user == 1:
                     update = input("Enter Patient Name:")
                 elif user ==2:
@@ -81,15 +90,19 @@ def UpdatePatDes():
                 elif user ==3:
                     update = input("Enter Patient Contact Number:")
                 elif user ==4:
-                    Receptionist()
+                    print("Bring you back to receptionist menu...")
                 else:
                     print("Invalid Response, Please try again.")
-                    Receptionist()
+                    
 
                 if update == "":
                     print("Error:Input cannot be blank.")
+                elif info[user] == update:
+                    print("Error, new value must be different from old value.")
                 else:
-                    confirm = input(f"System will be overwrite {info[user]} with {update}.\n1.Confirm\n2.Abort\nAre you sure?:")
+                    confirm = input(f"============================\nSystem will be overwrite [{info[user]}] with [{update}].\n1.Confirm\n2.Abort\nAre you sure?:")
+                    
+                    
                     match confirm:
                         case "1":
                             patientInfo.remove([info[0], info[1], info[2], info[3]])
@@ -97,19 +110,51 @@ def UpdatePatDes():
                             patientInfo.append([info[0], info[1], info[2], info[3]])
                             fileManager.writeFile("patient.txt", 4, patientInfo)
                             print("Updated")
-                            Receptionist()
+                                
                         case "2":
-                            Receptionist()
+                             print("Bring you back to receptionist menu...")
+                                
                         case _:
                             print("Error, Invalid Input.")
-                            Receptionist()
+                                
 
         if userid != info[0]: 
-            print(f"Patient ID {userid} does not exist.")
-            Receptionist()
+            print(f"Error, Patient ID {userid} does not exist.")
+            
+    Receptionist()
 
-    return
 def MakeAppoint():
+    patientInfo = fileManager.readFile("Appointment.txt")
+    userid = input("============================\nEnter Patient ID: ")
+    if userid == "" or userid.isdigit() == False:
+        print("Invalid input, ID must be digits and cannot be null.")
+    else:
+        for info in patientInfo:
+            if info[0] == userid:
+                print(f"User found.")
+                user = input("============================\nWhat would you like to do?\n1.Schedule Appointment\n2.View Patient's Appointment\n3.Cancel Appointment\n4.Abort\nYour Choice:")
+                if user == "" or user.isdigit() == False:
+                    print("Invalid input, ID must be digits and cannot be null.")
+                    Receptionist()
+                else:
+                    user = int(user)
+
+
+                if user == 1:
+                    date = input("Enter the date in DD/MM/YY,seperated by '/'")
+                    
+
+                    if info[1] == "Null":
+                        info[2] = []
+                elif user ==2:
+                    update = input("Enter Patient Password:")
+                elif user ==3:
+                    update = input("Enter Patient Contact Number:")
+                elif user ==4:
+                    print("Bring you back to receptionist menu...")
+                else:
+                    print("Invalid Response, Please try again.")
+
     print("Hello")
     return
 def RepPay():
